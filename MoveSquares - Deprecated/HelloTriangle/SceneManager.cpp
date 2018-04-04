0c804ff9eea7812f26d93041561292b7f54a40d2
gcolling <colling.g2@gmail.com>
2018-04-04 19:25:35 -0300

    Add files via upload

---
+++ MoveSquares - Deprecated/HelloTriangle/SceneManager.cpp
@@ -0,0 +1,239 @@
+#include "SceneManager.h"
+
+//static controllers for mouse and keyboard
+static bool keys[1024];
+static bool resized;
+static GLuint width, height;
+
+SceneManager::SceneManager()
+{
+}
+
+SceneManager::~SceneManager()
+{
+}
+
+void SceneManager::initialize(GLuint w, GLuint h)
+{
+	width = w;
+	height = h;
+	
+	// GLFW - GLEW - OPENGL general setup -- TODO: config file
+	initializeGraphics();
+
+}
+
+void SceneManager::initializeGraphics()
+{
+	// Init GLFW
+	glfwInit();
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	window = glfwCreateWindow(width, height, "Hello Transform", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	//Setando a callback de redimensionamento da janela
+	glfwSetWindowSizeCallback(window, resize);
+	
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewInit();
+
+	// Build and compile our shader program
+	addShader("../shaders/transformations.vs", "../shaders/transformations.frag");
+
+	//setup the scene -- LEMBRANDO QUE A DESCRIÇÃO DE UMA CENA PODE VIR DE ARQUIVO(S) DE 
+	// CONFIGURAÇÃO
+	setupScene();
+
+	resized = true; //para entrar no setup da câmera na 1a vez
+
+}
+
+void SceneManager::addShader(string vFilename, string fFilename)
+{
+	shader = new Shader (vFilename.c_str(), fFilename.c_str());
+}
+
+
+void SceneManager::key_callback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
+	}
+}
+
+void SceneManager::resize(GLFWwindow * window, int w, int h)
+{
+	width = w;
+	height = h;
+	resized = true;
+
+	// Define the viewport dimensions
+	glViewport(0, 0, width, height);
+}
+
+
+void SceneManager::do_movement()
+{
+	if (keys[GLFW_KEY_ESCAPE])
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (keys[GLFW_KEY_UP])
+		y_axis += 0.001f;
+
+	if (keys[GLFW_KEY_DOWN])
+		y_axis -= 0.001f;
+
+	if (keys[GLFW_KEY_LEFT])
+		x_axis -= 0.001f;
+
+	if (keys[GLFW_KEY_RIGHT])
+		x_axis += 0.001f;
+}
+
+void SceneManager::render()
+{
+	// Clear the colorbuffer
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Render scene
+	shader->Use();
+
+	// Create transformations 
+	//model = glm::mat4();
+	//model = glm::rotate(model, (GLfloat)glfwGetTime(), glm::vec3(0.0f, 0.0f, 0.5f));
+
+	// Get their uniform location
+	GLint modelLoc = glGetUniformLocation(shader->Program, "model");
+
+	if (resized) //se houve redimensionamento na janela, redefine a projection matrix
+	{
+		setupCamera2D();
+		resized = false;
+	}
+
+	GLint colorLoc = glGetUniformLocation(shader->Program, "inColor");
+	assert(colorLoc > -1);
+	glUseProgram(shader->Program);
+
+	float x = 0.0f, y = 0.0f;
+	float col_r = 1.0f, col_g = -1.0f, col_b = 0.0f;
+
+	for (int i = 0; i < 3; i++) {
+		y_axis = 0.0f;
+		for (int j = 0; j < 3; j++) {
+			model = glm::mat4();
+			model = glm::translate(model, glm::vec3(x_axis, y_axis, 0.0));
+
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glUniform4f(colorLoc, col_r, col_g, col_b, 1.0);
+
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+			y_axis += 0.5;
+			col_r -= 0.3;
+			col_g += 0.4;
+			col_b += 0.1;
+		}
+		x_axis += 0.5;
+	}
+}
+
+void SceneManager::run()
+{
+	//GAME LOOP
+	while (!glfwWindowShouldClose(window))
+	{
+		
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		//Update method(s)
+		do_movement();
+
+		//Render scene
+		render();
+		
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+}
+
+void SceneManager::finish()
+{
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+}
+
+
+void SceneManager::setupScene()
+{
+
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] = {
+		// Positions          // Colors           
+		-2.0f, -2.0f, 0.0f,   1.0f, 0.0f, 0.0f,    // Top Right
+		-2.0f, -1.5f, 0.0f,   0.0f, 1.0f, 0.0f,    // Bottom Right
+		-1.5f, -1.5f, 0.0f,   0.0f, 0.0f, 1.0f,   // Bottom Left
+		-1.5f, -2.0f, 0.0f,   1.0f, 1.0f, 0.0f,   // Top Left
+	};
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3, // First Triangle
+		1, 2, 3  // Second Triangle
+	};
+
+	GLuint VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+}
+
+void SceneManager::setupCamera2D()
+{
+	//corrigindo o aspecto
+	float ratio;
+	float xMin = -2.0, xMax = 2.0, yMin = -2.0, yMax = 2.0, zNear = -2.0, zFar = 2.0;
+	if (width >= height)
+	{
+		ratio = width / (float)height;
+		projection = glm::ortho(xMin*ratio, xMax*ratio, yMin, yMax, zNear, zFar);
+	}
+	else
+	{
+		ratio = height / (float)width;
+		projection = glm::ortho(xMin, xMax, yMin*ratio, yMax*ratio, zNear, zFar);
+	}
+
+	// Get their uniform location
+	GLint projLoc = glGetUniformLocation(shader->Program, "projection");
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+}
---
+++ MoveSquares - Deprecated/shaders/transformations.frag
@@ -0,0 +1,9 @@
+#version 430 core
+
+uniform vec4 inColor;
+out vec4 color;
+
+void main()
+{
+	color = inColor;
+}
